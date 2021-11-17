e8567385b4576e2f14b28c4b48788c6990856718
unknown <angell_2104@hotmail.com>
2021-11-16 19:01:10 -0600

    database connection

---
+++ database/database.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"github.com/angelAL21/fiber-postgres/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+// connectDb
+func ConnectDb() {
+	dsn := "host=localhost user=postgres password='abcde' dbname=godb port=5432 sslmode=disable TimeZone=America/Mexico"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("connected")
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("running migrations")
+	db.AutoMigrate(&models.Book{})
+
+	DB = Dbinstance{
+		Db: db,
+	}
+}
